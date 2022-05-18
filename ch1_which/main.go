6024bf6e3052e8790096eed98938076f17d60dcf
Chris Dunphy <chris@megaparsec.ca>
2022-05-18 14:25:11 -0600

    Completed exercises 1 and 2 for ch1_which

--- ch1_which/main.go
+++ ch1_which/main.go
@@ -12,20 +12,21 @@ func main() {
 		fmt.Println("Please provide an argument")
 		return
 	}
-	file := arguments[1]
 
-	path := os.Getenv("PATH")
-	pathSplit := filepath.SplitList(path)
-	for _, directory := range pathSplit {
-		fullPath := filepath.Join(directory, file)
-		fileInfo, err := os.Stat(fullPath)
-		if err == nil {
-			mode := fileInfo.Mode()
-			// Is it a regular file?
-			if mode.IsRegular() {
-				if mode&0111 != 0 {
-					fmt.Println(fullPath)
-					return
+	for ix := 1; ix != len(arguments); ix++ {
+		file := arguments[ix]
+		path := os.Getenv("PATH")
+		pathSplit := filepath.SplitList(path)
+		for _, directory := range pathSplit {
+			fullPath := filepath.Join(directory, file)
+			fileInfo, err := os.Stat(fullPath)
+			if err == nil {
+				mode := fileInfo.Mode()
+				// Is it a regular file?
+				if mode.IsRegular() {
+					if mode&0111 != 0 {
+						fmt.Println(fullPath)
+					}
 				}
 			}
 		}
