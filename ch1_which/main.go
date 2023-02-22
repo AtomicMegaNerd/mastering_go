663a1d84e97900df4f1366e367325773f4f265e0
Chris Dunphy <chris@megaparsec.ca>
2023-02-22 15:23:12 -0700

    Added some comments

--- ch1_which/main.go
+++ ch1_which/main.go
@@ -16,11 +16,16 @@ func main() {
 	for ix := 1; ix != len(arguments); ix++ {
 		file := arguments[ix]
 		path := os.Getenv("PATH")
+		// This splits the path into a list of strings so we can search each
 		pathSplit := filepath.SplitList(path)
+		// For each directory in the list of paths
 		for _, directory := range pathSplit {
+			// Get the full path
 			fullPath := filepath.Join(directory, file)
+			// See if the file exists at all
 			fileInfo, err := os.Stat(fullPath)
 			if err == nil {
+				// The file exists but it is a directory or a symlink?
 				mode := fileInfo.Mode()
 				// Is it a regular file?
 				if mode.IsRegular() {
